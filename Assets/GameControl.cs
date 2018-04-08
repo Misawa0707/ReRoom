b74655d92b1eb1527fd7c1d86c269105430399e9
misawa <guiyusanze40@gmail.com>
2018-04-08 23:36:33 +0900

    map

---
+++ Assets/GameControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public GameObject[] map;
+
+	// Use this for initialization
+	void Start () {
+        
+        int number = Random.Range(0, map.Length);
+        Instantiate(map[number], transform.position, transform.rotation);
+        Instantiate(map[Random.Range(0, map.Length)], transform.position, transform.rotation);
+        Instantiate(map[Random.Range(0, map.Length)], transform.position, transform.rotation);
+        Instantiate(map[Random.Range(0, map.Length)], transform.position, transform.rotation);
+        Instantiate(map[Random.Range(0, map.Length)], transform.position, transform.rotation);
+        Instantiate(map[Random.Range(0, map.Length)], transform.position, transform.rotation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
